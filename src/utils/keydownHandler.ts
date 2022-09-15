fda3bda575c62f19ad16614a308f44e5e708ad90
timmy <timhansher@gmail.com>
2022-09-14 19:44:28 -0500

    readme

--- src/utils/keydownHandler.ts
+++ src/utils/keydownHandler.ts
@@ -4,6 +4,7 @@ export const keydownHandler = (e: KeyboardEvent) => {
     const regex = /[a-zA-Z]/;
     if (store.input.length === 0) {
       if (regex.test(e.key) && e.key.length === 1) {
+        store.clock = false
         store.input = e.key
       }
     }
