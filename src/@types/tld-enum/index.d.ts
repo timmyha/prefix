90fc34c8eb82a7ad07803d1077ef6134116b6145
timmy <timhansher@gmail.com>
2022-09-14 19:37:33 -0500

    readme

---
+++ src/@types/tld-enum/index.d.ts
@@ -0,0 +1,10 @@
+declare module 'tld-enum' {
+import tldEnum from 'tld-enum'
+
+  interface Options {
+    rootDir: string;
+    rootPath: string;
+    gzip: boolean;
+  }
+
+  export default tldEnum 
