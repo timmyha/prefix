7592b226bcd3352406a4ac3575fc30cf174e6d55
timmy <timhansher@gmail.com>
2022-09-15 20:49:22 -0500

    readme

--- src/utils/handleQuery.ts
+++ src/utils/handleQuery.ts
@@ -1,17 +1,22 @@
-export const handleQuery = (e, filter, snap, validateUrl) => {
-    try {
-      if (e.key === "Enter") {
-        filter[0]
-          ? (window.location.href = filter[0].url)
-          : snap.input[0] === "!"
-          ? window.location.href = `https://duckduckgo.com/?q=${snap.input}`
-          : validateUrl(snap.input)
-          ? (window.location.href = `https://${snap.input}`)
-          : !validateUrl(snap.input) &&
-            (window.location.href = `${snap.prefixData.user.search}${snap.input}`)
-      }
-    } catch (error) {
-      console.warn("malformed URI, using search engine.");
-      window.location.href = `https://www.google.com/search?&q=${snap.input}`;
+export const handleQuery = (
+  e: any,
+  filter: any,
+  snap: any,
+  validateUrl: any
+) => {
+  try {
+    if (e.key === "Enter") {
+      filter[0]
+        ? (window.location.href = filter[0].url)
+        : snap.input[0] === "!"
+        ? (window.location.href = `https://duckduckgo.com/?q=${snap.input}`)
+        : validateUrl(snap.input)
+        ? (window.location.href = `https://${snap.input}`)
+        : !validateUrl(snap.input) &&
+          (window.location.href = `${snap.prefixData.user.search}${snap.input}`);
     }
-}
+  } catch (error) {
+    console.warn("malformed URI, using search engine.");
+    window.location.href = `https://www.google.com/search?&q=${snap.input}`;
+  }
+};
