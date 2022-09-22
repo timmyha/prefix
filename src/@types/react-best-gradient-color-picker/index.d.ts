41377761a2060caf6a4edbfc09de2377ebf5459b
timmy <timhansher@gmail.com>
2022-09-22 07:30:55 -0500

    added type for color picker

---
+++ src/@types/react-best-gradient-color-picker/index.d.ts
@@ -0,0 +1,11 @@
+declare module 'react-best-gradient-color-picker' {
+    import ColorPicker from 'react-best-gradient-color-picker'
+
+    interface Options {
+        rootDir: string
+        rootPath: string
+        gzip: boolean
+    }
+
+    export default ColorPicker
+}
